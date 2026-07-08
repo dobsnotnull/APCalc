b7bda255bd622cb61930271512c7c917cb3f2805
Doubrá <apresaidoubra@gmail.com>
2026-07-08 03:57:58 +0100

    javascript updates

--- READme.md
+++ READme.md
@@ -1,10 +1,10 @@
 //Things I wan't to add
 
 - Hover States and animationa for the buttons - CSS ✅
+- Match the weight of operator buttons to match the the numbers ✅
 - Make the buttons work - Vanilla Js
 - Make the input space like and actual calculator - Js
 - History - nodejs//MongoDB
-- Match the weight of operator buttons to match the the numbers
 
 //Stack
 
---
+++ calc.js
@@ -0,0 +1,52 @@
+// Firstly in Js , use these getElementId and querySelectorAll to find the display area and butttons that would be used in the code
+
+const display = document.getElementById("display");
+
+const buttons = document.querySelectorAll("button");
+
+
+
+// Add event listeners, in this case look for clicks
+
+// Now everytime you click any of the numbers, it shows in your console in our browser
+
+// The display.value thing displays the dataset of the button in the display area. += add the value to what is being displayed
+
+buttons.forEach((button) => {
+
+    button.addEventListener("click", () => {
+
+        const value = button.dataset.value;
+
+
+
+        // Condition for AC to clear the display area
+
+        if (value === "clear") {
+
+            display.value = "";
+
+        }
+
+        // Backspace condition
+
+        else if (value === "backspace") {
+
+            display.value = display.value.slice(0, -1);
+
+        }
+
+        else {
+
+            display.value += value;
+
+        }
+
+
+    })
+});
+
+
+
+// Make the display show what you click
+
